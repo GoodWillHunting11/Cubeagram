b17aaa0d3ae77d0fa02c0984f6acbc85dfabf18f
aaron <aaronshort19@gmail.com>
2022-02-25 12:40:21 -0600

    added error validations for username, image url, and email length

--- Cubeagram/app/forms/signup_form.py
+++ Cubeagram/app/forms/signup_form.py
@@ -19,15 +19,30 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def username_length(form, field):
+    username = field.data
+    if len(username) > 40:
+        raise ValidationError('Must be 40 characters or less')
+
 def validate_image(form, field):
     web = field.data
     if "http" not in web or "." not in web:
         raise ValidationError('Please enter a valid image url.')
 
+def validate_email_length(form, field):
+    email = field.data
+    if len(email) > 255:
+        raise ValidationError('Must be 255 characters or less')
+
+def validate_image_url_length(form,field):
+    image = field.data
+    if len(image) > 255:
+        raise ValidationError('Must be 255 characters or less')
+
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    imageUrl = StringField('imageUrl', validators=[DataRequired(), validate_image])
+        'username', validators=[DataRequired(), username_exists, username_length])
+    email = StringField('email', validators=[DataRequired(), user_exists, validate_email_length])
+    imageUrl = StringField('imageUrl', validators=[DataRequired(), validate_image, validate_image_url_length])
     password = StringField('password', validators=[DataRequired()])
